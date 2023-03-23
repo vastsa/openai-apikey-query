58f08456fad6556a94b1eea3fa3980f649943598
moeyys <58381898+moeyys@users.noreply.github.com>
2023-03-23 10:50:05 +0800

    use vercel proxy OpenAI API

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 import axios from "axios";
 const result: any = ref([]);
 const keys = ref('');
-const baseUrl = 'https://ai.mdzx.me/';
+const baseUrl = '/openai/';
 
 function formatDate(today: Date) {
   let year:any = today.getFullYear();
